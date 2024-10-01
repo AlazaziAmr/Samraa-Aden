c6c224aa89ffa9eaf7db6a4c51687ae71a423667
AlazaziAmr <alazaziamr@gmail.com>
2024-10-02 00:36:56 +0300

    Revert "All done."
    This reverts commit e046034a0113c46f5cd9196d9a681e01cc21eba2.

--- category/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
-
-class CategoryConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'category'
-    verbose_name = _('category')
--- category/sitemaps.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.sitemaps import Sitemap
-from django.core.paginator import Paginator
-from django.urls import reverse
-from .models import Category
-
-class CategorySitemap(Sitemap):
-    def items(self):
-        return Category.objects.all()
